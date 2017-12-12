74b95a300852223a4dae5fbdb3a97722181cb99a
Julien Deniau <julien.deniau@gmail.com>
2017-12-12 20:33:58 +0000

    add neovim init symlink

--- install.sh
+++ install.sh
@@ -1,8 +1,16 @@
+# Remove old vimrc file
 if [ -f ~/.vimrc ]
 then
     rm ~/.vimrc
 fi
+
+# Link vimrc file
+## vim
 ln -s ~/.vim/init.vim ~/.vimrc
+## neovim 
+mkdir -p ~/.config/nvim/
+ln -s ~/.vim/init.vim ~/.config/nvim/init.vim
+
 cd ~/.vim
 git submodule update --init bundle/Vundle.vim
 vim +PluginInstall +qall
